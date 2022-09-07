301abba8d5b402ee3dbf398a37138785e0f97e8c
Daryl Stark <daryl@dstark.nl>
2022-09-07 11:28:33 +0200

    Started with arguments

--- src/nbcli.py
+++ src/nbcli.py
@@ -1,8 +1,29 @@
 """ Module that contains the main method for the CLI
     application """
 
+from argparse import ArgumentParser
+
 def nbcli() -> None:
-    pass
+    # Create a argument parser
+    arguments = ArgumentParser('Netbox CLI')
+
+    # Create a object for subparser
+    subs = arguments.add_subparsers(
+        title='group',
+        metavar='group',
+        help='Command group',
+        dest='group',
+        required=True)
+
+    # Add the subparser for organization
+    organization = subs.add_parser('organization', help='Organization management').add_subparsers(
+        title='command', metavar='command', help='Command', required=True)
+    organization_sites = organization.add_parser('sites', help='Site management')
+
+    # Parse the arguments
+    args = arguments.parse_args()
+
+    print(args.command)
 
 if __name__ == '__main__':
     nbcli()
