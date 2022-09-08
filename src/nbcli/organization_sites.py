f6b08684268770847a6870ccca62793ecdee08b3
Daryl Stark <daryl@dstark.nl>
2022-09-08 21:09:35 +0200

    Finished the site retrieval

--- src/nbcli/organization_sites.py
+++ src/nbcli/organization_sites.py
@@ -1,5 +1,8 @@
+from ctypes import alignment
 from .nbcli import nbcli_object
 from .organization import organization
+from .cli import cli, console, tables
+from rich.table import Table
 
 
 @organization.group(help='Site management')
@@ -22,7 +25,26 @@ def list():
     # Create the object
     nbcli_object.create_pynetbox_object()
 
-    # Get the sites
+    # Create a table for the output
+    table = Table(**tables)
+    table.add_column('ID', style='item_identification', justify='right')
+    table.add_column('Name', style='item_identification')
+    table.add_column('Status')
+    table.add_column('Region')
+    table.add_column('Group')
+    table.add_column('Description')
+
+    # Get the resources and add them to the table
     resources = nbcli_object.nb.dcim.sites.all()
     for resource in resources:
-        print(resource)
+        table.add_row(
+            str(resource.id),
+            str(resource.name),
+            str(resource.status),
+            str(resource.region),
+            str(resource.group),
+            str(resource.description)
+        )
+
+    # Print the table
+    console.print(table)
